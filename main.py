6e046fc40beb1e20c94edaeaf1a6b6f1bc7604a4
y-sumi <63985771+y-sumi@users.noreply.github.com>
2020-04-21 09:33:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, abort
-#環境変数取得用
+# -*- coding: utf-8 -*-
 import os
-
+import sys
+from account_response import Response
+from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -13,46 +14,51 @@ from linebot.models import (
 )
 
 app = Flask(__name__)
+-----追加-----
+# インスタンス生成
+res = Response()
+---ここまで---
 
-# オリジナルの処理
-# line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-# handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+# Herokuの変数からトークンなどを取得
+channel_secret = os.environ['LINE_CHANNEL_SECRET']
+channel_access_token = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
 
-#環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
 
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-
-@app.route("/")
-def hello_world():
-    return "hello world!"
-
-@app.route("/callback", methods=['POST'])
+# LINEからのWebhook
+@app.route("/callback", methods = ['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # リクエストヘッダーから署名検証のための値を取得
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
+    # リクエストボディを取得
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
+    # 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
-
-@handler.add(MessageEvent, message=TextMessage)
+# LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合
+# reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
+# 第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
+@handler.add(MessageEvent, message = TextMessage)
 def handle_message(event):
+    #入力された内容(event.message.text)に応じて返信する
     line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
+    event.reply_token,
+    TextSendMessage(text = os.environ[res.getResponse(event.message.text)])
+    )
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
