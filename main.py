1928793d10bd47b109377b931cecbd480c91e797
y-sumi <63985771+y-sumi@users.noreply.github.com>
2020-04-24 09:26:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-# -*- coding: utf-8 -*-
+import urllib.request
 import os
 import sys
-from account_response import Response
+import json
+import scrape as sc
+from argparse import ArgumentParser
+
 from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
@@ -14,14 +17,9 @@ from linebot.models import (
 )
 
 app = Flask(__name__)
-#-----追加-----
-# インスタンス生成
-res = Response()
-#---ここまで---
 
-# Herokuの変数からトークンなどを取得
-channel_secret = os.environ['LINE_CHANNEL_SECRET']
-channel_access_token = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
@@ -32,33 +30,33 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
-# LINEからのWebhook
-@app.route("/callback", methods = ['POST'])
+
+@app.route("/callback", methods=['POST'])
 def callback():
-    # リクエストヘッダーから署名検証のための値を取得
     signature = request.headers['X-Line-Signature']
 
-    # リクエストボディを取得
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
+
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+
     return 'OK'
 
-# LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合
-# reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
-# 第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
-@handler.add(MessageEvent, message = TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    #入力された内容(event.message.text)に応じて返信する
+
+    word = event.message.text
+    result = sc.getNews(word)
+
     line_bot_api.reply_message(
     event.reply_token,
-    TextSendMessage(text = os.environ[res.getResponse(event.message.text)])
+    TextSendMessage(text=result)
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    port = int(os.getenv("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
